6ca2d09465ce1cb4b9491ab70347aa69cac40316
LENOVO <mdzubeir2468@gmail.com>
2022-02-20 18:21:03 +0530

    program 1

---
+++ Assignment 3/src/assignmentprograms/Main.java
@@ -0,0 +1,15 @@
+package assignmentprograms;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println(5/0);
+		}
+	catch (Exception e) {
+        System.out.println("End of program!");
+	}
+}
+}
